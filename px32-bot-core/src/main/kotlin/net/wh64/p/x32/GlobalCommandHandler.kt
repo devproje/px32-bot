e4afe685f7ed1001fb2b0b6610b8bf76e7a40039
Project_IO <me@projecttl.net>
2024-09-08 19:29:27 +0900

    feat: middle save

--- px32-bot-core/src/main/kotlin/net/wh64/p/x32/GlobalCommandHandler.kt
+++ px32-bot-core/src/main/kotlin/net/wh64/p/x32/GlobalCommandHandler.kt
@@ -1,5 +1,17 @@
 package net.wh64.p.x32
 
-class GlobalCommandHandler {
+import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 
+class GlobalCommandHandler : ListenerAdapter() {
+	override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+	}
+
+	override fun onUserContextInteraction(event: UserContextInteractionEvent) {
+	}
+
+	override fun onMessageContextInteraction(event: MessageContextInteractionEvent) {
+	}
 }
